ade04fd267a9f6c790e762bd51fc9ae8d7386e09
vitormoras <vitor.moras@hotmail.com>
2025-04-26 18:52:16 -0300

    arquivo requirements

--- pages/1_Consulta.py
+++ pages/1_Consulta.py
@@ -4,6 +4,9 @@ import pandas as pd
 st.title("Consulta de Clientes")
 st.divider()
 
+lateral = st.sidebar
+lateral.text("Páginas disponíveis")
+
 def carregar_arq():
     dados = pd.read_csv('clientes.csv')
     return dados
